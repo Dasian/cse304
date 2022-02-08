0f2b5a7230a484eb88d2055a7ee49709dd02e650
Sean Yang <60932078+s303824@users.noreply.github.com>
2022-02-07 22:59:14 -0500

    Working ssm_execute(), we need to look into error_edgecases.txt

--- hw1/ssm_scanner.py
+++ hw1/ssm_scanner.py
@@ -1,4 +1,5 @@
 # David Espiritu 112264228
+# Sean Yang sjyang 110766661
 # A script to implement an ssm interpreter
 # take as input the filename
 # outputs the result of execution
@@ -11,18 +12,20 @@ from os.path import exists
 # the stack to be used, just use the built in pop function
 stack = []
 # Key is the address, value is value
-store = {} 
+store = {}
 # list of instructions
 # every element is a tuple that contains the instruction and argument
 instructions = []
 # map labels to the index where instructions should be executed
 # ex: 'here': 2; execute the instruction starting at index 2
 labels = {}
-valid_instructions = ['ildc', 'iadd', 'isub', 'imul', 'idiv', 'imod', 'pop', 'dup', 'swap', 'jmp', 'jz', 'jnz', 'load', 'store']
+valid_instructions = ['ildc', 'iadd', 'isub', 'imul', 'idiv', 'imod', 'pop', 'dup', 'swap', 'jmp', 'jz', 'jnz', 'load',
+                      'store']
 no_arg_instructions = ['iadd', 'isub', 'imul', 'idiv', 'imod', 'pop', 'dup', 'swap', 'load', 'store']
 arg_instructions = ['ildc']
 jmp_instructions = ['jmp', 'jz', 'jnz']
 label_args = []
+
 
 # Instructions
 # ildc num - pushes num on the stack
@@ -30,7 +33,7 @@ label_args = []
 # isub - pop 2 elements, subtract, push difference
 # imul - pop 2 elements, multiply, push product
 # idiv - pop 2 elements, divide, push quotient
-# imod - find the remainer of 2 elements
+# imod - find the remainder of 2 elements
 # pop
 # dup - duplicate top most entry
 # swap - swap the top 2 elements
@@ -39,12 +42,91 @@ label_args = []
 # jnz label- pop stack, jmp if not 0
 # load - pop stack, push value at that address in the store
 # store - top element is integer i and second from top is address
-#  Put integer i at the adress index in the store
+#  Put integer i at the address index in the store
 def ssm_execute():
-	for i in range(0, len(instructions)):
-		inst = instructions[i][0]
-		arg = instructions[i][1]
-	# TODO
+    index = 0
+    while index < len(instructions):
+        inst = instructions[index][0]
+        arg = instructions[index][1]
+        if inst == "ildc":
+            stack.append(arg)
+        elif inst == "iadd":
+            if len(stack) < 2:
+                error("Cannot access requested value(s) from stack")
+            first_operand = stack.pop()
+            second_operand = stack.pop()
+            stack.append(second_operand + first_operand)
+        elif inst == "isub":
+            if len(stack) < 2:
+                error("Cannot access requested value(s) from stack")
+            first_operand = stack.pop()
+            second_operand = stack.pop()
+            stack.append(second_operand - first_operand)
+        elif inst == "imul":
+            if len(stack) < 2:
+                error("Cannot access requested value(s) from stack")
+            first_operand = stack.pop()
+            second_operand = stack.pop()
+            stack.append(second_operand * first_operand)
+        elif inst == "idiv":
+            if len(stack) < 2:
+                error("Cannot access requested value(s) from stack")
+            first_operand = stack.pop()
+            second_operand = stack.pop()
+            stack.append(second_operand // first_operand)
+        elif inst == "imod":
+            if len(stack) < 2:
+                error("Cannot access requested value(s) from stack")
+            first_operand = stack.pop()
+            second_operand = stack.pop()
+            stack.append(second_operand % first_operand)
+        elif inst == "pop":
+            if len(stack) < 1:
+                error("Cannot access requested value(s) from stack")
+            stack.pop()
+        elif inst == "dup":
+            if len(stack) < 1:
+                error("Cannot access requested value(s) from stack")
+            stack.append(stack[-1])
+        elif inst == "swap":
+            if len(stack) < 2:
+                error("Cannot access requested value(s) from stack")
+            top = stack.pop()
+            bottom = stack.pop()
+            stack.append(top)
+            stack.append(bottom)
+        elif inst == "jz":
+            if len(stack) < 1:
+                error("Cannot access requested value(s) from stack")
+            jump_value = stack.pop()
+            if jump_value == 0:
+                index = labels.get(arg) - 1
+        elif inst == "jnz":
+            if len(stack) < 1:
+                error("Cannot access requested value(s) from stack")
+            jump_value = stack.pop()
+            if jump_value != 0:
+                index = labels.get(arg) - 1
+        elif inst == "jmp":
+            index = labels.get(arg) - 1
+        elif inst == "load":
+            if len(stack) < 1:
+                error("Cannot access requested value(s) from stack")
+            address = stack.pop()
+            if store.get(address) is None:
+                error("Cannot access requested value from store")
+            stack.append(store.get(address))
+        elif inst == "store":
+            if len(stack) < 2:
+                error("Cannot access requested value(s) from stack")
+            element = stack.pop()
+            address = stack.pop()
+            store.update({address: element})
+        index = index + 1
+
+
+# TODO
+# Look into error_edgecases.txt, all other test cases show the interpreter working as intended
 
 # Takes as input a filename and parses the file
 # Assumes that filename checks have already been done
@@ -58,163 +140,169 @@ def ssm_execute():
 # deal with label as the last statement. What is mapped to in the labels dictionary?
 # can labels be the last statement, can a label declaration be the only statement, case of empty file?
 def ssm_scan(filename):
-	# open 
-	global f
-	f = open(filename)
+    # open
+    global f
+    f = open(filename)
 
-	# remove comments and make the file one continuous string
-	file_string = ''
-	for l in f.readlines():
-		# removing comments
-		if '#' in l:
-			l = l.replace(l[l.find('#'):l.find('\n')], '')
-		file_string += l
+    # remove comments and make the file one continuous string
+    file_string = ''
+    for l in f.readlines():
+        # removing comments
+        if '#' in l:
+            l = l.replace(l[l.find('#'):l.find('\n')], '')
+        file_string += l
 
-	# create a list of consecutive characters (words) separated by 1+ whitespace
-	file_string = file_string.replace('\n', ' ')
-	file_string = file_string.replace('\t', ' ')
-	words = file_string.split(' ')
-	while '' in words:
-		words.remove('')
+    # create a list of consecutive characters (words) separated by 1+ whitespace
+    file_string = file_string.replace('\n', ' ')
+    file_string = file_string.replace('\t', ' ')
+    words = file_string.split(' ')
+    while '' in words:
+        words.remove('')
 
-	# deal with empty files
-	if len(words) == 0:
-		scan_error("Empty File")
+    # deal with empty files
+    if len(words) == 0:
+        error("Empty file")
 
-	print(words)
+    print(words)
 
-	# verify words (instructions, labels, integers, or label:instruction)
-	# prevents sequential label declarations
-	i = 0
-	isPrevLabel = False # is the previous word a label?
-	for word in words:
-		print(word)
-		# verify instructions and integers
-		if word in valid_instructions or isInt(word):
-			isPrevLabel = False
-		# verify label definition
-		elif isValidLabel(word) and not isPrevLabel:
-			isPrevLabel = True
-		# split label:instruction words
-		elif word.find(':') != len(words[i]) - 1 and not isPrevLabel:
-			split = word.split(':')
-			split[0] = split[0] + ':'
-			# verify label followed by instruction (no whitespace)
-			if len(split) == 2 and isValidLabel(split[0]) and split[1] in valid_instructions:
-				words.pop(i)
-				words.insert(i, split[0])
-				words.insert(i+1, split[1])
-				isPrevLabel = False
-				i += 1 # verified two words so counter must be updated
-			# verify label arguments
-			elif not isValidLabel(word + ':'):
-				scan_error("invalid word (split label)", [word, split, words])
-		else:
-			scan_error("invalid word (verify words)", [word])
-		i += 1
+    # verify words (instructions, labels, integers, or label:instruction)
+    # prevents sequential label declarations
+    i = 0
+    isPrevLabel = False  # is the previous word a label?
+    for word in words:
+        print(word)
+        # verify instructions and integers
+        if word in valid_instructions or isInt(word):
+            isPrevLabel = False
+        # verify label definition
+        elif isValidLabel(word) and not isPrevLabel:
+            isPrevLabel = True
+        # split label:instruction words
+        elif word.find(':') != len(words[i]) - 1 and not isPrevLabel:
+            split = word.split(':')
+            split[0] = split[0] + ':'
+            # verify label followed by instruction (no whitespace)
+            if len(split) == 2 and isValidLabel(split[0]) and split[1] in valid_instructions:
+                words.pop(i)
+                words.insert(i, split[0])
+                words.insert(i + 1, split[1])
+                isPrevLabel = False
+                i += 1  # verified two words so counter must be updated
+            # verify label arguments
+            elif not isValidLabel(word + ':'):
+                error("Invalid word (split label)", [word, split, words])
+        else:
+            error("Invalid word (verify words)", [word])
+        i += 1
+    print("\n")
+    # all words at this point should be valid
+    # argument checks and data structure insertion
+    isCurrArg = False  # does the current word need to be an argument?
+    prev_word = ''
+    for word in words:
+        # inserting arguments
+        if isCurrArg:
+            if prev_word in arg_instructions and isInt(word):
+                instructions.append((prev_word, int(word)))
+            elif prev_word in jmp_instructions and isValidLabel(word + ':'):
+                instructions.append((prev_word, word))
+                label_args.append(word)
+            else:
+                error("Invalid argument", [word])
+            isCurrArg = False
+        # inserting labels
+        elif isValidLabel(word):
+            word = word[0:-1]  # remove colon
+            curr_index = len(instructions)
+            labels.update({word: curr_index})
+            isCurrArg = False
+        # inserting instructions
+        elif word in no_arg_instructions:
+            instructions.append((word, None))
+            isCurrArg = False
+        elif word in arg_instructions or word in jmp_instructions:
+            isCurrArg = True
+        else:
+            error("Invalid word (verify order)", [word])
+        prev_word = word
 
-	# all words at this point should be valid
-	# argument checks and data structure insertion
-	isCurrArg = False # does the current word need to be an argument?
-	prev_word = ''
-	for word in words:
-		# inserting arguments
-		if isCurrArg:
-			if prev_word in arg_instructions and isInt(word):
-				instructions.append((prev_word, int(word)))
-			elif prev_word in jmp_instructions and isValidLabel(word + ':'):
-				instructions.append((prev_word, word))
-				label_args.append(word)
-			else:
-				scan_error("invalid argument", [word])
-			isCurrArg = False
-		# inserting labels
-		elif isValidLabel(word):
-			word = word[0:-1] # remove colon
-			curr_index = len(instructions)
-			labels.update({word:curr_index})
-			isCurrArg = False
-		# inserting instructions
-		elif word in no_arg_instructions:
-			instructions.append((word, None))
-			isCurrArg = False
-		elif word in arg_instructions or word in jmp_instructions:
-			isCurrArg = True
-		else:
-			scan_error("invalid word (verify order)", [word])
-		prev_word = word
+    # determines if label args point to established labels
+    verify_label_args()
+    f.close()
+    return
 
-	# determines if label args point to established labels 
-	verify_label_args()
-	f.close()
-	return
 
 # determines if a string literal i is an integer
 # An integer can be any number with an optional - in the first position
 def isInt(i):
-	if i.find('-') == 0 and i[1:len(i)].isdigit():
-		return True
-	else:
-		return i.isdigit()
+    if i.find('-') == 0 and i[1:len(i)].isdigit():
+        return True
+    else:
+        return i.isdigit()
+
 
 # Determines if str s is a valid label (end colon not removed)
 # Valid labels must start with an alphabetic character
 # followed by a sequence of alphabetic or numerica or underscore chars
 def isValidLabel(s):
-	# determines if : is at the end of the string
-	if s.find(':') == len(s) - 1:
-		s = s[0:-1]
-	else:
-		return False
-	# first character must be alphabetic
-	if not s[0].isalpha():
-		return False
-	for c in s:
-		if not (c.isalpha() or c.isdigit() or c == '_'):
-			scan_error("invalid label", [s])
-	return True
+    # determines if : is at the end of the string
+    if s.find(':') == len(s) - 1:
+        s = s[0:-1]
+    else:
+        return False
+    # first character must be alphabetic
+    if not s[0].isalpha():
+        return False
+    for c in s:
+        if not (c.isalpha() or c.isdigit() or c == '_'):
+            error("Invalid label", [s])
+    return True
+
 
 # Determines if all label arguments point to established labels
 def verify_label_args():
-	keys = labels.keys()
-	for arg in label_args:
-		if arg not in keys:
-			scan_error("invalid label", [arg])
+    keys = labels.keys()
+    for arg in label_args:
+        if arg not in keys:
+            error("Invalid label", [arg])
 
-# prints an scan_error message and optional list of arguments
-def scan_error(s, args = []):
-	print(s)
-	for a in args:
-		print(a)
-	f.close()
-	exit()
+
+# prints an error message and optional list of arguments
+def error(s, args=[]):
+    print(s)
+    for a in args:
+        print(a)
+    f.close()
+    exit()
+
 
 def main():
-	# take input from cmdline
-	# possibly could also mean using input() but idk
-	if len(sys.argv) != 2:
-		print("Usage:",sys.argv[0], "[filename]")
-		exit()
-	filename = sys.argv[1]
-	if not exists(filename):
-		scan_error("File not found")
+    # take input from cmdline
+    # possibly could also mean using input() but idk
+    if len(sys.argv) != 2:
+        print("Usage:", sys.argv[0], "[filename]")
+        exit()
+    filename = sys.argv[1]
+    if not exists(filename):
+        error("File not found")
 
-	# scan the file and place instructions into datastructures used for execution
-	# handles compile time scan_errors	
-	ssm_scan(filename)
+    # scan the file and place instructions into datastructures used for execution
+    # handles compile time input errors
+    ssm_scan(filename)
 
-	# debug
-	scan_error("Instructions and Labels", [instructions, labels])
+    # debug
+    # error("Instructions and labels", [instructions, labels])
 
-	# Execute the instructions
-	ssm_execute()
+    # Execute the instructions
+    ssm_execute()
 
-	# Print result (topmost stack element) and exit
-	if len(stack) == 0:
-		print('empty stack')
-		exit()
-	print(stack[-1])
-	exit()
+    # Print result (topmost stack element) and exit
+    if len(stack) == 0:
+        print('Empty stack')
+        exit()
+    print(stack[-1])
+    exit()
+
 
 if __name__ == "__main__":
-	main()
+    main()
