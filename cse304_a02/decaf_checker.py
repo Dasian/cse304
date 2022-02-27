1f13973217af8ae5b0900a7c8379b8f6f8be5703
Sean Yang <60932078+s303824@users.noreply.github.com>
2022-02-26 22:46:36 -0500

    2/26

--- cse304_a02/decaf_checker.py
+++ cse304_a02/decaf_checker.py
@@ -13,6 +13,20 @@ import sys
 from os.path import exists
 import ply.lex as lex
 import decaf_lexer
+import ply.yacc as yacc
+import decaf_parser
+
+
+def decaf_scan(filename):
+    return 0
+
+
+# Compute column
+#     input is the input text string
+#     token is a token instance
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
 
 
 def main():
@@ -25,12 +39,27 @@ def main():
         exit()
 
     lexer = lex.lex(module=decaf_lexer)
-    lexer.input("3 * 4 + (12 - 6/2)$")
+    global file
+    file = open(filename)
+
+    # make the file one continuous string
+    file_string = ''
+    multi_line_comment = False
+    for line in file.readlines():
+        file_string += line
+
+    lexer.input(file_string)
     while True:
         tok = lexer.token()
         if not tok:
             break  # No more input
         print(tok)
+
+    """parser = yacc.yacc(tabmodule=decaf_parser)
+    while True:
+        s = '3+2'
+        result = parser.parse(s)
+        print(result)"""
 
     print("Yes")
 
