a33528636f1d4253367f550e2986f1ae75223c6b
Sean Yang <60932078+s303824@users.noreply.github.com>
2022-03-01 14:03:32 -0500

    Parser

--- cse304_a02/decaf_checker.py
+++ cse304_a02/decaf_checker.py
@@ -12,56 +12,37 @@
 import sys
 from os.path import exists
 import ply.lex as lex
-import decaf_lexer
 import ply.yacc as yacc
-import decaf_parser
-
-
-def decaf_scan(filename):
-    return 0
-
-
-# Compute column
-#     input is the input text string
-#     token is a token instance
-def find_column(input, token):
-    line_start = input.rfind('\n', 0, token.lexpos) + 1
-    return (token.lexpos - line_start) + 1
 
 
 def main():
     if len(sys.argv) != 2:  # Takes input from cmdline
-        print("python3 decaf_checker.py [filename]")
+        print("USAGE: python3 decaf_checker.py [filename]")
         exit()
     filename = sys.argv[1]
     if not exists(filename):
         print("File not found")
         exit()
 
+    import decaf_lexer
+    import decaf_parser
     lexer = lex.lex(module=decaf_lexer)
-    global file
-    file = open(filename)
+    parser = yacc.yacc(module=decaf_parser, debug=1)
 
-    # make the file one continuous string
-    file_string = ''
-    multi_line_comment = False
-    for line in file.readlines():
-        file_string += line
+    file = open(filename, 'r')
+    file_string = file.read()
+    file.close()
 
+    '''
     lexer.input(file_string)
     while True:
         tok = lexer.token()
         if not tok:
             break  # No more input
-        print(tok)
+        print(tok)'''
 
-    """parser = yacc.yacc(tabmodule=decaf_parser)
-    while True:
-        s = '3+2'
-        result = parser.parse(s)
-        print(result)"""
-
-    print("Yes")
+    parser.parse(file_string, lexer=lexer)
+    print("YES")
 
 
 if __name__ == "__main__":
--- cse304_a02/decaf_lexer.py
+++ cse304_a02/decaf_lexer.py
@@ -3,12 +3,6 @@
 
 #  PLY/lex scanner specification file
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore = ' \t'
-t_ignore_COMMENT = r'\/\/.*'
-t_ignore_MULTILINE_COMMENT = r'\\\*(.*|\n)*\*\\'
-
-
 reserved = {
     'boolean': 'BOOLEAN',
     'break': 'BREAK',
@@ -35,16 +29,26 @@ reserved = {
     'while': 'WHILE'
 }
 
+tokens = list(reserved.values()) + ['ID', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+                                    'ASSIGN', 'EQUALITY', 'STRING_CONST', 'FLOAT_CONST',
+                                    'INT_CONST', 'NOT',
+                                    'GREATER', 'LESSER', 'GEQ', 'LEQ', 'INEQUALITY', 'AND', 'OR']
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ' \t'
+t_ignore_COMMENT = r'\/\/.*'
+
+# Variable used to keep track of columns for errors
+line_start = 1
+
 # Literal characters
-literals = ['{', '}', ';', '.', '!', ',']
+literals = ['(', ')', '{', '}', ';', '.', ',']
 
 # Regular expression rules for simple tokens
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
 t_ASSIGN = r'='
 t_EQUALITY = r'=='
 t_GREATER = r'>'
@@ -54,17 +58,27 @@ t_LEQ = r'<='
 t_INEQUALITY = r'!='
 t_AND = r'&&'
 t_OR = r'\|\|'
+t_NOT = r'!'
 
 
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+    global line_start
+    line_start = t.lexpos + 1
+
+
+# Define a rule so we can track line numbers within multi-line comments
+def t_multi_line_comment(t):
+    r'/\*(.|\n)*\*/'
+    t.lexer.lineno += t.value.count('\n')
+
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+    print("Illegal character %s at (%d, %d)" % (t.value[0], t.lexer.lineno, (t.lexpos - line_start) + 1))
+    exit()
 
 
 # Identifier rule
@@ -74,36 +88,22 @@ def t_ID(t):
     return t
 
 
-def t_LBRACE(t):
-    r'\{'
-    t.type = '{'  # Set token type to the expected literal
-    return t
-
-
-def t_RBRACE(t):
-    r'\}'
-    t.type = '}'  # Set token type to the expected literal
-    return t
-
-
+# String constants
 def t_STRING_CONST(t):
     r'\".*\"'
     t.value = t.value[1:len(t.value) - 1]
     return t
 
 
+# Float constants
 def t_FLOAT_CONST(t):
-    r'\d+(\.(\d+)?([eE][-+]?\d+)?|[eE][-+]?\d+)'
+    r'\d+.\d+'
     t.value = float(t.value)
     return t
 
 
+# Integer constants
 def t_INT_CONST(t):
     r'\d+'
     t.value = int(t.value)
     return t
-
-
-tokens = list(reserved.values()) + ['ID', 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-    'LPAREN', 'RPAREN', 'ASSIGN', 'EQUALITY', 'STRING_CONST', 'FLOAT_CONST', 'INT_CONST',
-    'GREATER', 'LESSER', 'GEQ', 'LEQ', 'INEQUALITY', 'AND', 'OR']
