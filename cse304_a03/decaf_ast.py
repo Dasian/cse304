11dbb46c7099c8f4e0c8ee7ee95fa93c693ac65e
Dasian <davidrespiritu99@gmail.com>
2022-03-31 16:25:27 -0400

    Updated initialization for lists and dictionaries

--- cse304_a03/decaf_ast.py
+++ cse304_a03/decaf_ast.py
@@ -7,33 +7,60 @@
 # All classes in this block are records
 # Part of Class Table
 class ClassRecord:
-    def __init__(self, name="", superName="", constructors=[], methods=[], fields=[]):
+    def __init__(self, name="", superName="", constructors=None, methods=None, fields=None):
         self.name = name # string
         self.superName = superName # string
-        self.methods = methods # list of MethodRecord objects
-        self.constructors = constructors # list of ConstructorRecord objects
-        self.fields = fields # list of FieldRecord objects
+        # list of MethodRecord objects
+        if methods is None:
+            self.methods = []
+        else:
+            self.methods = methods 
+        # list of ConstructorRecord objects
+        if constructors is None:
+            self.constructors = []
+        else:
+            self.constructors = constructors 
+        # list of FieldRecord objects
+        if fields is None:
+            self.fields = []
+        else:
+            self.fields = fields 
 
 class ConstructorRecord:
-    def __init__(self, id=-1, visibility="", parameters=[], variableTable=[], body=None):
+    def __init__(self, id=-1, visibility="", parameters=None, variableTable=None, body=None):
         self.id = id # unique int
         self.visibility = visibility # string; public/private
-        self.parameters = parameters # list of VariableRecord objects passed to the constructor
-        self.variableTable = variableTable # list of all VariableRecord objects (local vars + params)
         self.body = body # a single Statement object
+        # list of VariableRecord objects passed to the constructor
+        if parameters is None:
+            self.parameters = []
+        else:
+            self.parameters = parameters 
+        # list of all VariableRecord objects (local vars + params)
+        if variableTable is None:
+            self.variableTable = []
+        else:
+            self.variableTable = variableTable 
 
 class MethodRecord:
-    def __init__(self, name="", id=-1, containingClass="", visibility="", applicability="", body=None, variableTable=[], returnType=None, paramaters=[]):
+    def __init__(self, name="", id=-1, containingClass="", visibility="", applicability="", body=None, variableTable=None, returnType=None, parameters=None):
         self.name = name # string
         self.id = id # unique int
         self.containingClass = containingClass # string
         self.visibility = visibility # string; public/private
         self.applicability = applicability # string; static/non-static
         self.body = body # a single Statement object
-        self.variableTable = variableTable # list of VariableRecord objects
         self.returnType = returnType # a single TypeRecord object
-        self.paramaters = paramaters # list of VariableRecord objects passed to the method
-
+        # list of VariableRecord objects
+        if variableTable is None:
+            self.variableTable = []
+        else:
+            self.variableTable = variableTable
+        # list of VariableRecord objects passed to the constructor
+        if parameters is None:
+            self.parameters = []
+        else:
+            self.parameters = parameters
 class FieldRecord:
     def __init__(self, name="", id=-1, containingClass="", visibility="", applicability="", type=None):
         self.name = name # string
@@ -48,7 +75,7 @@ class VariableRecord:
     def __init__(self, name="", id=-1, kind="", type=None):
         self.name = name # string
         self.id = id # unique int
-        self.kind = kind # ??
+        self.kind = kind # string; formal/local
         self.type = type # a single TypeRecord object
 
 class TypeRecord:
@@ -59,10 +86,18 @@ class Statement:
     # kinds: If, While, For, Return, Expr, Block, Break, Continue, Skip
     # attributes: key is the attribute name and the value is mapped
     # @Sean for Block statements have the key in attributes named 'stmnts'
-    def __init__(self, lineRange=[], kind='', attributes={}):
-        self.lineRange = lineRange # 2 int list; [startLine, endLine]
+    def __init__(self, lineRange=None, kind='', attributes=None):
         self.kind = kind # string; see above for valid kinds
-        self.attributes = attributes # dict; key is attribute, value is mapped
+        # dict; key is attribute, value is mapped
+        if attributes is None:
+            self.attributes = {}
+        else:
+            self.attributes = attributes
+        # 2 int list; [startLine, endLine]
+        if lineRange is None:
+            self.lineRange = []
+        else:
+            self.lineRange = lineRange
 
 # If you want to nest expressions have a key that is 'Expression'
 #   that maps to a *list* of Expression object
@@ -76,7 +111,15 @@ class Statement:
 class Expression:
     # kinds: Constant, Var, Unary, Binary, Assign, Auto, Field-access, 
     #   Method-call, New-object, This, Super, Class-reference
-    def __init__(self, lineRange=[], kind='', attributes={}):
-        self.lineRange = lineRange # 2 int list; [startLine, endLIne]
+    def __init__(self, lineRange=None, kind='', attributes=None):
         self.kind = kind # string; see above
-        self.attributes = attributes # dict; key is attribute, value is mapped
+        # dict; key is attribute, value is mapped
+        if attributes is None:
+            self.attributes = {}
+        else:
+            self.attributes = attributes
+        # 2 int list; [startLine, endLine]
+        if lineRange is None:
+            self.lineRange = []
+        else:
+            self.lineRange = lineRange
